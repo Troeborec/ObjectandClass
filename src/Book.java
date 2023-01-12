c53435f04e03d1cf9502747500d44f1e266b16c2
Troeborec <defied_jeff@mail.ru>
2023-01-12 22:58:40 +1200

    THE END!

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+   private String name;
+  private   Author author;
+  private   int yearOfPublication;
+
+    public Book(String name, Author author, int yearOfPublication) {
+        this.name = name;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public void setYearOfPublication() {
+        this.yearOfPublication = yearOfPublication;
+    }
+    }
